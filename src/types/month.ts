6425b52adcfa161d048117c7303bd4ec10e5d859
Henrique Ferreira <henrique.fer@hotmail.com>
2023-06-13 21:08:33 -0300

    chore: criado tipagem e contexto para Mês

---
+++ src/types/month.ts
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export interface IMonth {
+  id: string
+  name: string
+}
+
+
+export class Month implements IMonth {
+  id: string
+  name: string
+
+  constructor(data?: any) {
+    if (data?.id) {
+      this.id = data.id
+    } else {
+      this.id = uuidv4()
+    }
+    this.name = data?.name
+  }
+}
+
+export type MonthsContextType = {
+  months: IMonth[]
+  addMonth: (month: IMonth) => void
+}
