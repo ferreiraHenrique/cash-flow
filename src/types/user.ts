7f0395c85260e4148d553cb04341d9ca4640fa1f
Henrique Ferreira <henrique.fer@hotmail.com>
2023-06-15 16:33:45 -0300

    chore: criada tipagem para usuário

---
+++ src/types/user.ts
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export interface IUser {
+  id: string
+  name: string
+  email: string
+  password: string
+}
+
+export class User implements IUser {
+  id: string
+  name: string
+  email: string
+  password: string
+
+  constructor(data: any) {
+    this.id = data.id || uuidv4()
+    this.name = data.name
+    this.email = data.email
+    this.password = data.password
+  }
+}
+
+export interface IRegisterUser {
+  name: string
+  email: string
+  password: string
+  register: () => IUser
+}
+
+export class RegisterUser implements IRegisterUser {
+  name: string;
+  email: string;
+  password: string;
+
+  constructor(data: any) {
+    this.name = data.name
+    this.email = data.email
+    this.password = btoa(data.password)
+  }
+
+  register(): IUser {
+    return new User(this)
+  };
+}
