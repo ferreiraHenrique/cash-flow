9f42b775e6d874bc9ba04d63f673b14b350ad1a4
Henrique Ferreira <henrique.fer@hotmail.com>
2023-06-15 14:19:49 -0300

    improve: add métodos de cálculo de totais do mês

--- src/types/month.ts
+++ src/types/month.ts
@@ -1,14 +1,20 @@
 import { v4 as uuidv4 } from 'uuid';
+import { ITransaction, Transaction } from './transaction';
 
 export interface IMonth {
   id: string
   name: string
+  transactions: ITransaction[]
+  calcTotalAmount: () => number
+  calcTotalDiscount: () => number
+  calcTotalSubtotal: () => number
 }
 
 
 export class Month implements IMonth {
   id: string
   name: string
+  transactions: ITransaction[];
 
   constructor(data?: any) {
     if (data?.id) {
@@ -17,12 +23,47 @@ export class Month implements IMonth {
       this.id = uuidv4()
     }
     this.name = data?.name
+
+    if (
+      data?.transactions &&
+      typeof data.transactions == 'object' &&
+      data.transactions.length
+    ) {
+      this.transactions = data.transactions.map((t:any) => new Transaction(t))
+    } else {
+      this.transactions = []
+    }
   }
+
+  public calcTotalAmount(): number {
+    return this.transactions.reduce((total: number, t: ITransaction) => {
+      if (t.isCredit) return total + t.amount
+      return total - t.amount
+    }, 0)
+  };
+
+  calcTotalDiscount(): number {
+    return this.transactions.reduce((total: number, t: ITransaction) => (
+      total + t.discount
+    ), 0)
+  }
+
+  calcTotalSubtotal(): number {
+    return this.transactions.reduce((total: number, t: ITransaction) => {
+      if (t.isCredit) return total + t.calcSubtotal()
+      return total - t.calcSubtotal()
+    }, 0)
+  };
 }
 
 export type MonthsContextType = {
   months: IMonth[]
+  monthSelected: IMonth | null
+  selectMonth: (month: IMonth) => void
   addMonth: (month: IMonth) => void
   removeMonth: (month: IMonth) => void
-  updateMonth: (month: IMonth) => void
+  updateMonth: (data?: any) => void
+  addTransaction: (transaction: ITransaction) => void
+  removeTransaction: (transaction: ITransaction) => void
+  updateTransaction: (transaction: ITransaction) => void
 }
