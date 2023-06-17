01261d2a351aac10950992cd13e9c6a95045518d
Henrique Ferreira <henrique.fer@hotmail.com>
2023-06-17 16:17:36 -0300

    improve: melhorias de layout

--- src/pages/meses/index.tsx
+++ src/pages/meses/index.tsx
@@ -1,7 +1,5 @@
 import MainLayout from "@/components/MainLayout";
 import MonthList from "@/components/MonthList";
-import NavBar from "@/components/NavBar";
-import SideMenu from "@/components/SideMenu";
 import ModalProvider from "@/contexts/ModalContext";
 import MonthsProvider from "@/contexts/MonthContext";
 import { GetServerSidePropsContext } from "next";
@@ -12,19 +10,13 @@ import { authOptions } from "../api/auth/[...nextauth]";
 export default function MesesPage() {
   return (
     <MainLayout>
-      <SideMenu />
-
-      <main className='relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl ml-72'>
-        <NavBar />
-
-        <div className='w-full px-6 py-6 mx-2'>
-          <MonthsProvider>
-            <ModalProvider>
-              <MonthList />
-            </ModalProvider>
-          </MonthsProvider>
-        </div>
-      </main>
+      <div className='w-full px-6 py-6 mx-2'>
+        <MonthsProvider>
+          <ModalProvider>
+            <MonthList />
+          </ModalProvider>
+        </MonthsProvider>
+      </div>
     </MainLayout>
   )
 }
