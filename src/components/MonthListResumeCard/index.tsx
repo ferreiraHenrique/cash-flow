d7e00fff87c266c77ef0e6b45bdc9ae5e4e4d045
Henrique Ferreira <henrique.fer@hotmail.com>
2023-06-21 17:43:16 -0300

    improve: exibição do esqueleto enquanto carrega dados

--- src/components/MonthListResumeCard/index.tsx
+++ src/components/MonthListResumeCard/index.tsx
@@ -2,13 +2,23 @@ import { useContext } from "react";
 import MonthResumeCard from "../MonthResumeCard";
 import { MonthsContext } from "@/contexts/MonthContext";
 import { MonthsContextType } from "@/types/month";
+import MonthResumeCardSkeleton from "../MonthResumeCard/skeleton";
 
 
 export default function MonthListResumeCard() {
-  const {months, selectMonth} = useContext(MonthsContext) as MonthsContextType
+  const {months, isLoading, selectMonth} = useContext(MonthsContext) as MonthsContextType
 
   return (
     <>
+      {
+        isLoading && (
+          <>
+            <MonthResumeCardSkeleton />
+            <MonthResumeCardSkeleton />
+            <MonthResumeCardSkeleton />
+          </>
+        )
+      }
       {months.map(m => (
         <MonthResumeCard
           key={m.id}
