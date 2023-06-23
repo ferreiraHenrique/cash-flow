eeb469be9022e880e173ee80cfbdec68022e6b9b
Henrique Ferreira <henrique.fer@hotmail.com>
2023-06-23 14:21:59 -0300

    chore: modal de ano

---
+++ src/components/ModalYear/new.tsx
@@ -0,0 +1,38 @@
+import { useContext, useRef } from "react";
+import Modal from "../Modal";
+import Swal from "sweetalert2";
+import { FormHandles } from "@unform/core";
+import ModalYearForm from "./form";
+import { YearsContext } from "@/contexts/YearContext";
+import { Year, YearsContextType } from "@/types/year";
+
+
+export default function ModalNewYear() {
+  const {addYear} = useContext(YearsContext) as YearsContextType
+
+  const formRef = useRef<FormHandles | null>(null)
+  const handleFormSubmit = async (data: any) => {
+    Swal.fire({text: 'Criando período', showConfirmButton: false})
+    Swal.showLoading()
+
+    const created = await addYear(new Year({...data}))
+    if (!created) {
+      Swal.fire("Ops", "não foi possível criar o período", "error")
+      return
+    }
+
+    Swal.close()
+  }
+
+  return <Modal
+    title="Criar novo período"
+    confirmButtonText="Adicionar período"
+    onConfirm={() => formRef?.current?.submitForm()}
+    content={
+      <ModalYearForm
+        formRef={formRef}
+        handleFormSubmit={handleFormSubmit}
+      />
+    }
+  />
+}
---
+++ src/components/ModalYear/update.tsx
@@ -0,0 +1,38 @@
+import { MonthsContextType } from "@/types/month";
+import Modal from "../Modal";
+import ModalMonthForm from "./form";
+import { useContext, useRef } from "react";
+import { MonthsContext } from "@/contexts/MonthContext";
+import Swal from "sweetalert2";
+import { FormHandles } from "@unform/core";
+
+
+export default function ModalUpdateMonth() {
+  const {monthSelected, updateMonth} = useContext(MonthsContext) as MonthsContextType
+
+  const formRef = useRef<FormHandles | null>(null)
+  const handleFormSubmit = async (data: any) => {
+    Swal.fire({text: 'Atualizando mês', showConfirmButton: false})
+    Swal.showLoading()
+
+    if (!await updateMonth(data)) {
+      Swal.fire("Ops", "não foi possível atualizar o mês", "error")
+      return
+    }
+
+    Swal.close()
+  }
+
+  return <Modal
+    title="Atualizar mês"
+    confirmButtonText="Salvar mês"
+    onConfirm={() => formRef?.current?.submitForm()}
+    content={
+      <ModalMonthForm
+        formRef={formRef}
+        handleFormSubmit={handleFormSubmit}
+        initialData={{name: monthSelected?.name}}
+      />
+    }
+  />
+}
